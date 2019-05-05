5492150dd77e38e3dc348f67460c26654905fc70
Alejandro <ale.bentivengo@hotmail.com>
2019-05-05 10:48:01 -0300

    Initial commit

--- Java/GameEngine/src/com/benti/execution/Game.java
+++ Java/GameEngine/src/com/benti/execution/Game.java
@@ -1,4 +1,108 @@
 package com.benti.execution;
 
-public class Game {
+import com.benti.execution.graphics.Screen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class Game extends Canvas implements Runnable {
+    public static String _VERSION = "0.1a";
+    public static int _WIDTH = 300;
+    public static int _HEIGHT = _WIDTH / 16 * 9;
+    public static int _SCALE = 3;
+
+    private JFrame frame;
+    private Thread thread;
+    private boolean running = false;
+
+    private Screen screen;
+
+    private BufferedImage image = new BufferedImage(_WIDTH, _HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+    public Game() {
+        Dimension size = new Dimension(_WIDTH * _SCALE, _HEIGHT * _SCALE);
+        setPreferredSize(size);
+
+        screen = new Screen(_WIDTH, _HEIGHT);
+
+        frame = new JFrame();
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this, "Game - Main Thread");
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        long lastTime = System.nanoTime();
+        final double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        int frames = 0;
+        int updates = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+        }
+        stop();
+    }
+
+    public void update() {
+
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        screen.clear();
+        screen.render();
+
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = screen.pixels[i];
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+
+        g.dispose();
+        bs.show();
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle("Game Engine - " + _VERSION);
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+
+        game.start();
+    }
 }
--- Java/GameEngine/src/com/benti/execution/graphics/Screen.java
+++ Java/GameEngine/src/com/benti/execution/graphics/Screen.java
@@ -1,4 +1,27 @@
 package com.benti.execution.graphics;
 
 public class Screen {
+
+    private int width, height;
+    public int[] pixels;
+
+    public Screen(int width, int height) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[width * height];
+    }
+
+    public void clear() {
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = 0;
+        }
+    }
+
+    public void render() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixels[x+y*width] = 0xff00ff;
+            }
+        }
+    }
 }
