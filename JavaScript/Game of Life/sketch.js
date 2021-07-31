58b6fa7f49987d9f2555357013cc63472f6e2645
Alejandro <ale.bentivengo@hotmail.com>
2021-07-31 20:28:12 +0100

    some fixes

--- JavaScript/Game of Life/sketch.js
+++ JavaScript/Game of Life/sketch.js
@@ -1,9 +1,10 @@
 let data;
-const cols = 100;
-const rows = 100;
+const cols = 50;
+const rows = 50;
 const _WIDTH = 800;
 const _HEIGHT = 800;
-let rectValues = { "height": _WIDTH / cols, "width": _HEIGHT / rows };
+const _FPS = 20;
+let rectValues = { "height": _HEIGHT / rows, "width": _WIDTH / cols };
 
 function createDataArray(cols, rows) {
   data = new Array(rows);
@@ -25,13 +26,15 @@ function getDataValue(value) {
 function setup() {
   createCanvas(_WIDTH, _HEIGHT, P2D);
   createDataArray(cols, rows);
-  frameRate(10);
+  frameRate(_FPS);
 }
 
 function draw() {
   background(255, 255, 255);
   drawData();
+  // As per wiki all cells need to be calculated with current values
   calculateNextValues();
+  // And then the changes must be applied once all was calculated
   applyChanges();
 }
 
@@ -58,14 +61,15 @@ function calculateNextValues() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       const neighbours = getNeighbours(x, y);
-      if (data[y][x].value === 1) {
-        if (neighbours.has(1)) {
-          data[y][x].nextValue = neighbours.get(1) < 2 || neighbours.get(1) > 3 ? 0 : 1;          
+      if (neighbours.has(1)) {
+        const liveNeighbours = neighbours.get(1);
+        if (data[y][x].value === 1) {
+          data[y][x].nextValue = liveNeighbours < 2 || liveNeighbours > 3 ? 0 : 1;
         } else {
-          data[y][x].nextValue = 0;
+          data[y][x].nextValue = liveNeighbours === 3 ? 1 : 0;
         }
       } else {
-        data[y][x].nextValue = neighbours.get(1) === 3 ? 1 : 0;
+        data[y][x].nextValue = 0;
       }
     }
   }
